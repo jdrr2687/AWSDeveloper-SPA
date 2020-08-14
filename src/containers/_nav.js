b99460e1cc87fc93d18e8928dd22bf1b010c219a
Daniel Rocha <jdrr2687@gmail.com>
2020-08-13 22:40:59 -0600

    Init

---
+++ src/containers/_nav.js
@@ -0,0 +1,23 @@
+import CIcon from '@coreui/icons-react'
+
+export default [
+
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Enviar Mensaje con SES',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Subir Archivo a S4',
+    to: '/theme/colors',
+    icon: 'cil-drop',
+  },
+
+  {
+    _tag: 'CSidebarNavDivider',
+    className: 'm-2'
+  }
+]
+
